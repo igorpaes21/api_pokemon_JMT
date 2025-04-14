2e103678228f11428215e8a7cc10f3396abfc609
igorpaes21 <irsp2121@hmail.com>
2025-04-14 19:18:43 -0300

    first commit

---
+++ src/main/java/br/senai/pokedex/api_pokemon/ApiPokemonApplication.java
@@ -0,0 +1,13 @@
+package br.senai.pokedex.api_pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonApplication.class, args);
+	}
+
+}
