f3ae4f44deaa6361affebf96e8e4b1115f589683
igorpaes21 <irsp2121@hmail.com>
2025-04-14 19:18:50 -0300

    first commit

---
+++ src/test/java/br/senai/pokedex/api_pokemon/ApiPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.pokedex.api_pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
